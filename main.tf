a627e5bbb9d8112f94513396c6e4f997c7a846aa
TARAK <tarakaramtirumala@gmail.com>
2023-03-31 16:04:36 +0530

    changes

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ resource "aws_elasticache_cluster" "elasticache" {
   engine_version    = var.engine_version
   port              = 6379
   subnet_group_name = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
+
 
   tags = merge(
     var.tags,
